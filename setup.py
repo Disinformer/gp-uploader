9632c27e04a9b768a7152df0215982c888ffa2da
Disinformer <41363136+Disinformer@users.noreply.github.com>
2024-12-06 23:43:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='gp-uploader-custom',
-    version='0.0.1',
+    version='0.0.2',
     description='Script to monitor a directory and upload any existing files to google photo using adb connected android device',
     author='Disinformer',
     url='https://github.com/Disinformer/gp-uploader',
